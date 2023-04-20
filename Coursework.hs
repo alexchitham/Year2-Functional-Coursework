c4ff32bde7e154863facda2f6162cc4d3a353bcb
alexchitham <alexchitham@hotmail.co.uk>
2023-04-20 17:51:22 +0100

    Part 4  finished

--- Coursework.hs
+++ Coursework.hs
@@ -1,4 +1,3 @@
-import GHC.Exts.Heap (GenClosure(var))
 ------------------------- Auxiliary functions
 
 merge :: Ord a => [a] -> [a] -> [a]
@@ -149,18 +148,22 @@ step I = []
 step K = []
 step S = []
 step (I :@ x) = x : [I :@ x' | x' <- step x]
-step (K :@ x :@ y) = x : [K :@ x' :@ y | x' <- step x] 
-  ++ [K :@ x :@ y' | y' <- step y]
-step (S :@ x :@ y :@ z) = (x :@ z :@ (y :@ z)) : [S :@ x' :@ y :@ z | x' <- step x]
-  ++ [S :@ x :@ y' :@ z | y' <- step y]
-  ++ [S :@ x :@ y :@ z' | z' <- step z]
-step (x :@ y) = [x' :@ y | x' <- step x]
-  ++ [x :@ y' | y' <- step y]  
-
+step (K :@ x :@ y) =
+  x
+    : [K :@ x' :@ y | x' <- step x]
+    ++ [K :@ x :@ y' | y' <- step y]
+step (S :@ x :@ y :@ z) =
+  (x :@ z :@ (y :@ z))
+    : [S :@ x' :@ y :@ z | x' <- step x]
+    ++ [S :@ x :@ y' :@ z | y' <- step y]
+    ++ [S :@ x :@ y :@ z' | z' <- step z]
+step (x :@ y) =
+  [x' :@ y | x' <- step x]
+    ++ [x :@ y' | y' <- step y]
 
 run :: Combinator -> IO ()
 run x = do
-  print x 
+  print x
   let xs = step x
   if null xs
     then return ()
@@ -169,12 +172,11 @@ run x = do
 ------------------------- Assignment 2: Combinators to Lambda-terms
 
 toLambda :: Combinator -> Term
-toLambda (c :@ d) = Apply (toLambda c) (toLambda d) 
-toLambda I = Lambda  "x" (Variable "x")
+toLambda (c :@ d) = Apply (toLambda c) (toLambda d)
+toLambda I = Lambda "x" (Variable "x")
 toLambda K = Lambda "x" (Lambda "y" (Variable "x"))
 toLambda S = Lambda "x" (Lambda "y" (Lambda "z" (Apply (Apply (Variable "x") (Variable "z")) (Apply (Variable "y") (Variable "z")))))
 toLambda (V x) = Variable x
-
 
 ------------------------- Assignment 3: Lambda-terms to Combinators
 
@@ -195,13 +197,31 @@ toCombinator (Apply m n) = toCombinator m :@ toCombinator n
 ------------------------- Assignment 4: Estimating growth
 
 sizeL :: Term -> Int
-sizeL = undefined
+sizeL (Apply m n) = 1 + sizeL m + sizeL n
+sizeL (Lambda x m) = 1 + sizeL m
+sizeL (Variable x) = 1
 
 sizeC :: Combinator -> Int
-sizeC = undefined
+sizeC (c :@ d) = 1 + sizeC c + sizeC d
+sizeC I = 1
+sizeC K = 1
+sizeC S = 1
+sizeC (V x) = 1
 
 series :: Int -> Term
-series = undefined
+series n = lambdaPart n n
+  where
+    lambdaPart :: Int -> Int -> Term
+    lambdaPart p q
+      | p == 0 = Lambda (variables !! p) (applyPart q)
+      | p >= 1 = Lambda (variables !! p) (lambdaPart (p-1) q)
+      | p < 0 = error "Must be at least 0"
+
+    applyPart :: Int -> Term 
+    applyPart n 
+      | n > 0 = Apply (applyPart (n-1)) (Variable (variables !! n))
+      | n == 0 = Variable (variables !! n)
+        
 
 ------------------------- Assignment 5: Optimised interpretation
 
